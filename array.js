34e2441411746e58e09e8c3580c39027c4d02731
bobbydowling6 <bobbydowling6@gmail.com>
2017-10-18 16:11:50 -0500

    made changes

--- array.js
+++ array.js
@@ -251,27 +251,41 @@ var exponent = function(a, n)
 
 console.log(exponent(8, 2));
 //Problem 4
-function merge_sort(left_part, right_part)
-{
-    var i = 0;
-    var j = 0;
-    var results = [];
+// Split the array into halves and merge them recursively
+function mergeSort (arr) {
+  if (arr.length === 1) {
+    // return once we hit an array with a single item
+    return arr
+  }
 
-    while (i < left_part.length || j < right_part.length) {
-        if (i === left_part.length) {
-            // j is the only index left_part
-            results.push(right_part[j]);
-            j++;
-        }
-        else if (j === right_part.length || left_part[i] <= right_part[j]) {
-            results.push(left_part[i]);
-            i++;
-        } else {
-            results.push(right_part[j]);
-            j++;
-        }
-    }
-    return results;
+  const middle = Math.floor(arr.length / 2) // get the middle item of the array rounded down
+  const left = arr.slice(0, middle) // items on the left side
+  const right = arr.slice(middle) // items on the right side
+
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
 }
 
-console.log(merge_sort([1,3,4], [3,7,9]));
+// compare the arrays item by item and return the concatenated result
+function merge (left, right) {
+  let result = []
+  let indexLeft = 0
+  let indexRight = 0
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      result.push(left[indexLeft])
+      indexLeft++
+    } else {
+      result.push(right[indexRight])
+      indexRight++
+    }
+  }
+
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+}
+
+const list = [34, 7, 23, 32, 5, 62]
+console.log(mergeSort(list));
